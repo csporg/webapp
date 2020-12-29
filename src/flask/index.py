d211eea0c68600590b5fe20813f75179fc009411
cloudstones <70410709+cloudstones@users.noreply.github.com>
2020-12-30 04:03:06 +0530

    Update index.py

--- src/flask/index.py
+++ src/flask/index.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 application = Flask(__name__)
 
 #configure db
-application.config['MYSQL_HOST'] = '172.31.90.196'
+application.config['MYSQL_HOST'] = '192.168.1.7'
 application.config['MYSQL_USER'] = 'krishna'
 application.config['MYSQL_PASSWORD'] = 'Krishna_123'
 application.config['MYSQL_DB'] = 'indigo'
